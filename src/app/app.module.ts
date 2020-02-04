0c3ae5ae1a5f09cdc02fa220340da0120a1482a3
dhruv gajwa <dhruvg.dakshana16@gmail.com>
2020-02-04 15:00:33 +0530

    CHange
    c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC4z3NOANzrcOAm827YxAP3llXDt8fr-WM',
-  authDomain: 'pushnotification-4f41a.firebaseapp.com',
-  databaseURL: 'https://pushnotification-4f41a.firebaseio.com',
-  projectId: 'pushnotification-4f41a',
-  storageBucket: 'pushnotification-4f41a.appspot.com',
-  messagingSenderId: '980001533855',
-  appId: '1:980001533855:web:e0f444e6274cc99e3697aa',
-  measurementId: 'G-W5EMTE0M1V'
+  
 };
 
 @NgModule({
